294740a24d07494af807301b132bc57c856ce6f5
Wagner Alvarado <warnerxp@outlook.com>
2019-06-05 11:50:36 -0600

    Models Done

---
+++ NorthwindSolution/Northwind.Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Northwind.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime MyProperty { get; set; }
+        public string OrderNumber { get; set; }
+        public int CustomerId { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ NorthwindSolution/Northwind.Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Northwind.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Roles { get; set; }
+        public string Password { get; set; }
+    }
+}
