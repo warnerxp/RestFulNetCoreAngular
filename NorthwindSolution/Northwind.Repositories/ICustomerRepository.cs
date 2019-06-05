a0a17ef95ced5e28044b680fde7b87e2041ddfdb
Wagner Alvarado <warnerxp@outlook.com>
2019-06-05 15:06:34 -0600

    repositories

---
+++ NorthwindSolution/Northwind.Repositories/ICustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿using Northwind.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Northwind.Repositories
+{
+    public interface ICustomerRepository:IRepository<Customer>
+    {
+        IEnumerable<Customer> CustomerPagedList(int page, int rows);
+    }
+}
